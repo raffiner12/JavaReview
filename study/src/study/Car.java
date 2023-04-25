d725571da64a9c7a6e434f2d619045081cbf9595
raffiner12 <1soigne2@gmail.com>
2023-04-25 22:02:25 +0900

    메소드 정의 & 호출

--- study/src/study/Car.java
+++ study/src/study/Car.java
@@ -7,6 +7,12 @@ class Car {
 	int num;
 	// 연료 양
 	double gas;
+	
+// 차량 정보를 표시하는 메소드 정의
+	void show() {
+		System.out.println("차량 번호는 " + num + "입니다.");
+		System.out.println("연료 양은 " + gas + "입니다.");
+	}
 }
 
 // 자동차 클래스의 객체를 생성하기
@@ -20,8 +26,9 @@ class Sample1 {
 		car1.num = 1234;
 		car1.gas = 20.5;
 		
-		System.out.println("차량 번호는 " + car1.num + "입니다.");
-		System.out.println("연료 양은 " + car1.gas + "입니다.");
+		// System.out.println("차량 번호는 " + car1.num + "입니다.");
+		// System.out.println("연료 양은 " + car1.gas + "입니다.");
+		car1.show();
 		
 		// 두 번째 객체
 		Car car2 = new Car();
