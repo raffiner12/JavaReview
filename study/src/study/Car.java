85f11257b4d7f0907ba53d621d1050fd571fe484
raffiner12 <1soigne2@gmail.com>
2023-04-25 20:48:05 +0900

    객체 생성하기

--- study/src/study/Car.java
+++ study/src/study/Car.java
@@ -1,9 +1,18 @@
 package study;
 
+// 자동차 클래스
 class Car {
-// 필
+// 필드
 	// 차량 번호
 	int num;
 	// 연료 양
 	double gas;
+}
+
+// 자동차 클래스의 객체를 생성하기
+class Sample1 {
+	public static void main(String[] args) {
+		Car car1;
+		car1 = new Car();
+	}
 }
